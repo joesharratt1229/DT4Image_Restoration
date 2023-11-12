a485471023ab70341b2f1ab26ede5567dd5c22ab
joesharratt1229 <joesharratt1229@gmail.com>
2023-11-12 12:24:40 +0000

    Added code for pretrained denoiser

---
+++ denoiser/base.py
@@ -0,0 +1,34 @@
+import os
+import torch
+import torch.nn as nn
+from .model.unet import UNet
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+class UNetDenoiser2D(nn.Module):
+    def __init__(self) -> None:
+        super(UNetDenoiser2D, self).__init__()
+
+        self.check_path = os.path.join(CURRENT_DIR, 'weights', 'pretrained_denoiser.pt')
+        net = UNet(2, 1)
+        net.load_state_dict(state_dict = self.check_path)
+        net.eval()
+
+        for param in net.parameters():
+            param.requires_grad = False
+        
+        self.net = net
+
+    
+    def forward(self,
+                x: torch.Tensor,
+                sigma: torch.Tensor
+                ) -> torch.Tensor:
+        
+        N, C, H, W = x.shape
+        sigma = sigma.view(N, 1, 1, 1)
+
+        noise_map = torch.ones(N, 1, H, W).to(x.device) * sigma
+        out = self.net(torch.cat([x, noise_map], dim = -1))
+
+        return torch.clamp(out, 0, 1)
