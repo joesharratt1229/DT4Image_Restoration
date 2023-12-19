6aaa1f84b9fb065507f74a94a93185396c558db2
joesharratt1229 <joesharratt1229@gmail.com>
2023-12-19 16:47:42 +0000

    added train file

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from trainer.base import MetaTrainer
 
+
 if __name__ == '__main__':
 
-    trainer = MetaTrainer(batch_size = 8)
+    trainer = MetaTrainer(batch_size = 48)
+
+
 
     trainer.train()
-    
+
+
+
 
