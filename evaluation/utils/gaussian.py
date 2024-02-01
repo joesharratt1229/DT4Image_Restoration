2af6bd3708d33fa87134648b19c3c32f34dd1187
joesharratt1229 <joesharratt1229@gmail.com>
2024-02-01 23:10:40 +0000

    utility functions

---
+++ evaluation/utils/gaussian.py
@@ -0,0 +1,25 @@
+from typing import Any
+import torch
+import numpy as np
+
+class ContinuousNoise:
+    def __init__(self, 
+                 minimum_noise: int = 5, 
+                 maximum_noise: int = 20
+                 ) -> None:
+        
+        self.minimum_noise = minimum_noise
+        self.maxiumum_noise = maximum_noise
+
+    
+    def __call__(self, x, noise):
+        assert (noise > self.minimum_noise) & (noise < self.maxiumum_noise), 'Value Error: Invalid noise level'
+
+        sigma = noise/255
+        sigma = torch.ones_like(x) * sigma
+        sigma.to(x.device)
+        y = x + torch.randn(*x.shape) * sigma
+
+        return y, sigma
+
+        
