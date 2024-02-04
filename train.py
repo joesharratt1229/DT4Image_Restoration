7dae151799deb83c727c2c933c7c4da315c1399c
joesharratt1229 <joesharratt1229@gmail.com>
2024-02-04 14:47:42 +0000

    added boiler plate training config

--- train.py
+++ train.py
@@ -12,17 +12,11 @@ from contextlib import nullcontext
 
 
 from transformer.decision_transformer import DecisionTransformer, DecisionTransformerConfig
-from dataset.training import TrainingDataset
+from dataset.datasets import TrainingDataset
 
 """
 SET PARAMETERS AND HYPERPARAMETERS HERE
 """
-
-device_type = 'cuda' if torch.cuda.is_available() else 'cpu'
-dtype = 'bloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16'
-ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
-ctx = nullcontext() if device_type == 'cpu' else torch.amp.autoscast(device_type = device_type, dtype = ptdtype)
-
 
 def ddp_setup(rank, world_size):
     """
@@ -34,6 +28,7 @@ def ddp_setup(rank, world_size):
     os.environ['MASTER_PORT'] = '12355'
     init_process_group(backend = 'nccl', rank = rank, world_size = world_size)
     torch.cuda.set_device(rank)
+
 
 def prepare_dataloader(dataset: Dataset, batch_size: int):
     return DataLoader(
@@ -55,12 +50,17 @@ class TrainerConfig:
 class Trainer:
     def __init__(self,
                  model: torch.nn.Module,
+                 train_config,
                  train_data_loader : DataLoader,
+                 optimizer: torch.optim,
                  gpu_id: int,
                  save_every: int,
                  compile: bool = False) -> None:
         
+        self.config = train_config
+        
         self.gpu_id = gpu_id
+        self.optimizer = optimizer
         self.model = model.to(gpu_id)
         if compile:
             self.model = torch.compile(model)
@@ -70,52 +70,71 @@ class Trainer:
 
 
     def _run_batch(self, trajectory):
-        self.optimizer.zero_grad()
-        states, actions, rtg, traj_masks, task, timesteps, output_masks = trajectory
+        states, actions, rtg, traj_masks, timesteps= trajectory
         actions_target = torch.clone(actions).detach()
         with ctx:
-            actions_preds, _ = self.model(actions, rtg, states, timesteps, task, output_masks)
-        actions_preds = actions_preds.view(-1, actions_preds.shape[-1])[traj_masks.view(-1, traj_masks.shape[-1]) > 0]
-        actions_target = actions_target.view(-1, actions_target.shape[-1])[traj_masks.view(-1, traj_masks.shape[-1]) > 0]
-        loss = F.mse_loss(actions_preds, actions_target)
+            actions_preds, _ = self.model(actions, rtg, states, timesteps)
+            actions_preds = actions_preds.view(-1, actions_preds.shape[-1])[traj_masks.view(-1, traj_masks.shape[-1]) > 0]
+            actions_target = actions_target.view(-1, actions_target.shape[-1])[traj_masks.view(-1, traj_masks.shape[-1]) > 0]
+            loss = F.mse_loss(actions_preds, actions_target)
         loss.backward()
-        nn.utils.clip_grad.clip_grad_norm_(self.model.parameters(), self.config.gradient_clipping)
+        nn.utils.clip_grad.clip_grad_norm_(self.model.parameters(), self.config.grad_norm_clipping)
         self.optimizer.step()
+        self.optimizer.zero_grad(set_to_none = True)
 
     
     def _save_checkpoint(self):
-        ckp = self.model.module.state_dict()
+        raw_model = self.model.module if model.module else model
+        ckp = raw_model.module.state_dict()
         PATH = "checkpoints/model.pt"
         torch.save(ckp, PATH)
 
     
     def _run_epoch(self):
-        ### do something with model if DDP
+        ### do somethiisplang with model if DDP
         for trajectory in self.train_data_loader:
             self._run_batch(trajectory)
 
     def train(self):
-        model = self.model
-        raw_model = model.module if hasattr(self.model, 'module') else model
-        optimizer = model.configure_optimizers(config)
-        for epoch in range(self.config.max_parameters):
+        for epoch in range(self.config.max_epochs):
             self._run_epoch()
             if self.gpu_id == 0 and epoch % self.save_every == 0:
                 self._save_checkpoint()
 
 
+train_dict = {
+    'learning_rate' : 3e-4,
+    'beta' :(0.09, 0.95),
+    'weight_decay' : 0.1,
+    'max_epochs': 10,
+    'grad_norm_clipping': 0.1,
+    'num_workers': 0,
+    'batch_size': 16,
+    'block_size': 4
+}
+
+device_type = 'cuda' if torch.cuda.is_available() else 'cpu'
+dtype = 'bloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16'
+ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
+ctx = nullcontext() if device_type == 'cpu' else torch.amp.autoscast(device_type = device_type, dtype = ptdtype)
         
 
-
-model_config = DecisionTransformerConfig()
+train_config = TrainerConfig(**train_dict)
+model_config = DecisionTransformerConfig(block_size = train_config.block_size)
 model = DecisionTransformer(model_config)
+raw_model = model.module if hasattr(model, 'module') else model
+optimizer = model.configure_optimizers(train_config)
+dataset = TrainingDataset(block_size = train_config.block_size)
 
+data_loader = prepare_dataloader(dataset, train_config.batch_size)
+
+def main(rank: int, world_size: int, save_every: int):
+    ddp_setup(rank, world_size)
+
+#SPAWN MULTIPLE PROCESSES
 
 #if __name__ == '__main__':
 
 #    trainer.train()
 #    trainer = MetaTrainer(batch_size = args.batch_size)
-
-
-
 
